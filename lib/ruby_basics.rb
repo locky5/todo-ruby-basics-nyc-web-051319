5c375a7879e7b90fa30b59159a990f3791591e60
Kevin Dai <daikevin19@yahoo.com>
2019-04-20 08:03:12 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,20 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
-
+def greeting(greeting,name)
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
